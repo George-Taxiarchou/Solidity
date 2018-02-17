7c2cad5914a18a0c35e0b6cdefccbefbcde2492f
Gtax <gtax@Gtaxs-MacBook-Pro.local>
2018-02-17 16:41:42 +0200

    fetching accounts from  ganache

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const solc =r equire('solc');
+const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname,'contracts','inbox.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
