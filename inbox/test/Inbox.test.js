2b3a75fef56ce2f04a3cb26a61d2338049f6ffc2
Gtax <gtax@Gtaxs-MacBook-Pro.local>
2018-02-20 17:24:39 +0200

    test update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 =  new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface,bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
 	//Get a list off all accounts
@@ -12,10 +17,20 @@ beforeEach(async () => {
 
 	// Use one of those accounts to deploy
 	// the contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data:bytecode,arguments:['Hi there!'] })
+		.send({ from:accounts[0], gas:'1000000'})
+
+	inbox.setProvider(provider);
 });
 
 describe('Inbox',()=>{
 	it('deploys a contract',()=>{
-		console.log(accounts);
+		assert.ok(inbox.options.address);
+	});
+
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call();
+		assert.equal(message,'Hi there!');
 	});
 });
