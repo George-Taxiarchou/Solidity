5541173b32c54353f35d74aa822402e5ea38d637
George-Taxiarchou <drdarky@hotmail.com>
2018-02-20 17:40:31 +0200

    testing

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
+//test
 const provider = ganache.provider();
 const web3 = new Web3(provider);
 
@@ -28,7 +28,7 @@ describe('Inbox',()=>{
 	it('deploys a contract',()=>{
 		assert.ok(inbox.options.address);
 	});
-
+  
 	it('has a default message', async () => {
 		const message = await inbox.methods.message().call();
 		assert.equal(message,'Hi there!');
