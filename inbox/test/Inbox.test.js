645a717cb03b0d9faa50d905c9706a0a74e6fb04
George-Taxiarchou <drdarky@hotmail.com>
2018-02-20 17:43:02 +0200

    test update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -16,6 +16,21 @@ beforeEach(async () => {
 		
 
 	// Use one of those accounts to deploy
-assert.equal(message,'Hi there!');
+	// the contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data:bytecode,arguments:['Hi there!'] })
+		.send({ from:accounts[0], gas:'1000000'})
+
+	inbox.setProvider(provider);
+});
+
+describe('Inbox',()=>{
+	it('deploys a contract',()=>{
+		assert.ok(inbox.options.address);
+	});
+  
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call();
+		assert.equal(message,'Hi there!');
 	});
 });
